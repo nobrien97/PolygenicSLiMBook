7cf1355212f21809b7928bebd70acec5a0520a34
nobrien97 <nicholasobrien97@gmail.com>
2021-05-13 15:13:57 +1000

    Updated slimInParallel

---
+++ src/Parallelisation/Python/slim_sublaunch_combos.py
@@ -0,0 +1,36 @@
+from os import system
+from multiprocessing import Pool
+from itertools import product
+from pandas import read_csv, DataFrame
+from joblib import Parallel, delayed
+
+# Open the seeds file as a list
+
+seeds = read_csv(r'../Inputs/seeds.csv')['Seed'].to_list()
+
+# Create a list of parameters and generate unique combinations
+param1 = [0.1, 0.2, 0.3]
+param2 = ['Low', 'Medium', 'High']
+p = {'param1' : [0.1, 0.2, 0.3], 'param2' : ['Low', 'Medium', 'High']}
+keys, values = zip(*p.items())
+# https://stackoverflow.com/a/61335465/13586824
+combos = [dict(zip(keys, v)) for v in product(*values)]
+combos = DataFrame.from_dict(combos)
+
+
+# Open a new 'pool' - like makeCluster() in R
+
+cluster = Pool()
+
+# Do an operation on the pool - this is like foreach() in R
+
+def slim_call(seed, par1, par2):
+    system('slim -s {se} -d param1={p1} -d param2={p2} ~/Desktop/example_script.slim'.format(se=seed, p1=par1, p2=par2)) 
+
+seeds['Seed'].to_list()
+
+if __name__ == "__main__":
+    cluster.starmap(slim_call, )
+    cluster.close()
+    cluster.join()
+
---
+++ src/Parallelisation/Python/slim_sublaunch_seeds.py
@@ -0,0 +1,22 @@
+from os import system
+from multiprocessing import Pool
+from pandas import read_csv
+
+# Open the seeds file
+
+seeds = read_csv(r'../Inputs/seeds.csv')
+
+# Open a new 'pool' - like makeCluster() in R
+
+cluster = Pool()
+
+# Do an operation on the pool - this is like foreach() in R
+
+def slim_call(seed):
+    system('slim -s {} ~/Desktop/example_script.slim'.format(seed)) 
+
+cluster.map(slim_call, seeds['Seed'].tolist())
+
+cluster.close()
+cluster.join()
+
