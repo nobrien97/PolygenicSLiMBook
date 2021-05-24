1d2b6fa2ee7ebdbc8187afa6f094bc55805d56a4
nobrien97 <nicholasobrien97@gmail.com>
2021-05-24 15:12:25 +1000

    Updated SLiM sublauncher C++ to work (?)

--- src/Parallelisation/Cpp/run_slim.cpp
+++ src/Parallelisation/Cpp/run_slim.cpp
@@ -1,5 +1,5 @@
 #include "stdlib.h"
-#include "stdio.h"
+#include <iostream>
 #include <utility>
 #include <vector>
 #include <fstream>
@@ -8,6 +8,7 @@
 #include <stdexcept>
 #include <thread>
 
+// read_csv outputs to a vector of pairs: each pair has a name (parameter name) and a vector of entries
 typedef std::vector<std::pair<std::string, std::vector<std::string>>> dframe;
 
 // read_csv function from: https://www.gormanalysis.com/blog/reading-and-writing-csv-files-with-cpp/
@@ -57,10 +58,12 @@ dframe read_csv(std::string filename, bool header) {
     return file;
 }
 
-void runSLiM(dframe combo, dframe seed) {
-    std::string param1 = combo[0].first.at(0);
+void runSLiM(dframe combo, dframe seeds) {
+    std::string seed = seeds[0].second.at(0);
+    std::string param1 = combo[0].second.at(0);
     std::string param2 = combo[1].second.at(0);
-    std::system(fprintf(stdout, "/home/$USER/SLiM/slim -s %s -d param1=%s -d param2=%s ~/Desktop/example_script.slim", seed.at(0), seed, param1, param2));
+    std::string callLine = "/home/$USER/SLiM/slim -s " + seed + " -d param1=" + param1 + " -d param2=" + param2 + " ~/Desktop/example_script.slim";
+    std::system(callLine.c_str());
 
 }
 
@@ -68,9 +71,9 @@ void runSLiM(dframe combo, dframe seed) {
 
 int main() {
     // Read the seeds and combos
-    std::vector<std::pair<std::string, std::vector<std::string>>> seeds = read_csv("./seeds.csv", true);
-    std::vector<std::pair<std::string, std::vector<std::string>>> combos = read_csv("./combos.csv", true);
-    std::system(fprintf(stdout, "/home/$USER/SLiM/slim -s %s -d param1=%f -d param2=%s  ~/Desktop/example_script.slim", seeds.at(i), seeds, df.p[i,]$param2, intern=T)));
-
+    dframe seeds = read_csv("./seeds.csv", true);
+    dframe combos = read_csv("./combos.csv", true);
+    std::thread thread_obj(runSLiM, combos, seeds);
+    thread_obj.join();
     return 0;
 }
