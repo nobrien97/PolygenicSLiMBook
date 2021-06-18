33125ae6f1a500fb15102c3f5efb4eb3f4e5ebf9
nobrien97 <nicholasobrien97@gmail.com>
2021-06-18 15:03:51 +1000

    Updated shiny app with css hackjob to be fixed

--- src/Hypercube/app.R
+++ src/Hypercube/app.R
@@ -2,6 +2,25 @@
 
 library(shiny)
 
+# Helper functions
+
+numericInputInline <-function(inputId, label, value = "") 
+{
+  div(type = "text/css",
+      style="display: inline-block;
+             padding-right: 40px;",
+      tags$label(label, `for` = inputId), 
+      tags$input(id = inputId, type = "numeric", value = value, class = ))
+}
+
+selectInputInline <- function(inputId, label)
+{
+  div(style="label.control-label, .selectize-control.single{ display: table-cell; text-align: center; vertical-align: middle; } .form-group { display: table-row;}",
+      tags$label(label, `for` = inputId),
+      tags$input(id = inputId, choices = ))
+}
+
+
 # Define UI
 
 ui <- fluidPage(
@@ -9,10 +28,10 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel("Options",
-      numericInput("nfactors",
+      sliderInput("nfactors",
                    h3("Number of hypercube parameters"),
-                   value = 3),
-      uiOutput("factors")),
+                   min = 1, max = 20, step = 1, value = 1),
+      uiOutput("factors"), width = 12),
     mainPanel()
   )
   
@@ -24,20 +43,23 @@ server <- function(input, output, session) {
   output$factors <- renderUI({
     numFactors <- as.integer(input$nfactors)
     lapply(1:numFactors, function(i) {
-      fluidRow(
-        column(width = 3,
-               numericInput(inputID = paste0("fac_min", i), label = h3(paste("Minimum for factor", i)),
-                  value = 0)),
-        column(width = 3,
-               numericInput(inputID = paste0("fac_max", i), label = h3(paste("Maximum for factor", i)),
-                            value = 1)),
-      )
-      selectInput(inputID = paste0("fac_type", i), label = h3(paste("Data type for factor", i)),
-                  choices = list("Integer" = 1,
-                                 "Float" = 2))
+      fluidRow(style = "text-align: center;
+                 vertical-align:top;",
+        div(h3(paste("Factor", i))),
+        div(selectInput(inputId = paste0("fac_type", i), label = h4(paste("Data type for factor", i)),
+                    list("Integer" = 1,
+                         "Float" = 2)), style = "display: inline-block; padding-right: 40px;"),
+        div(numericInput(inputId = paste0("fac_min", i), label = h4("Minimum"),
+                  value = 0), style = "display: inline-block; padding-right: 40px;"),
+        div(numericInput(inputId = paste0("fac_max", i), label = h4("Maximum"),
+                  value = 1), style = "display: inline-block; padding-right: 40px;"),
+        br(),
+        br()
+)
     })
   })
 }
+
 
 # Run the app
 
