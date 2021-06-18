25099b6081be3f45bf667aa6541dd33a4e7cde8b
nobrien97 <nicholasobrien97@gmail.com>
2021-06-18 17:30:50 +1000

    Fixed hacks, started LHC generation code

--- src/Hypercube/app.R
+++ src/Hypercube/app.R
@@ -1,6 +1,7 @@
 # Shiny app to generate hypercubes, display their information, and save them to file
 
 library(shiny)
+library(DoE.wrapper)
 
 # Helper functions
 
@@ -24,14 +25,57 @@ selectInputInline <- function(inputId, label)
 # Define UI
 
 ui <- fluidPage(
-  titlePanel("Latin hypercube generator and visualiser"),
+  titlePanel(h1("Latin hypercube generator and visualiser")),
   
   sidebarLayout(
-    sidebarPanel("Options",
+    sidebarPanel(
+      h2("Options"),
+      # Generate hypercube button
+      fluidRow(
+        column(width = 6, style = "margin-top: 25px;",
+               actionButton(inputId = "genButton",
+                            label = "Generate hypercube!",
+                            icon = icon("play")))
+      ),
+      br(),
+      # Save button and filepath entry
+      fluidRow(style = "display:inline-block;",
+               column(width = 10,
+                      textInput(inputId = "saveText",
+                                label = "Filepath",
+                                placeholder = "~/hypercube.csv")
+               ),
+               column(width = 2, style = "margin-top: 25px;",
+                      actionButton(inputId = "saveButton",
+                                   label = "Save file!",
+                                   icon = icon("save")))
+      ),
       sliderInput("nfactors",
                    h3("Number of hypercube parameters"),
                    min = 1, max = 20, step = 1, value = 1),
-      uiOutput("factors"), width = 12),
+      
+      # Number of runs and LHC type
+      fluidRow(
+        column(width = 6, inline = T,
+        numericInput("nruns",
+                    "Number of samples",
+                    value = 512, step = 1)),
+        column(width = 6, inline = T,
+        selectInput("lhctype", 
+                    "Sampling method",
+                    choices = list(
+                                "genetic",
+                                "improved",
+                                "maximin",
+                                "optimum",
+                                "random"
+                    ), selected = "maximin")
+      )),
+    
+      uiOutput("factors"),
+      
+
+    ),
     mainPanel()
   )
   
@@ -40,24 +84,55 @@ ui <- fluidPage(
 # Define server
 
 server <- function(input, output, session) {
+  
+  # Generate factors
   output$factors <- renderUI({
     numFactors <- as.integer(input$nfactors)
     lapply(1:numFactors, function(i) {
-      fluidRow(style = "text-align: center;
-                 vertical-align:top;",
-        div(h3(paste("Factor", i))),
-        div(selectInput(inputId = paste0("fac_type", i), label = h4(paste("Data type for factor", i)),
+
+      # Inputs and factor name
+      fluidRow(
+        column(width = 3, h3(paste("Factor", i), align = "left")),
+        column(width = 4,
+        selectInput(inputId = paste0("fac_type", i), label = h4("Data type"),
                     list("Integer" = 1,
-                         "Float" = 2)), style = "display: inline-block; padding-right: 40px;"),
-        div(numericInput(inputId = paste0("fac_min", i), label = h4("Minimum"),
-                  value = 0), style = "display: inline-block; padding-right: 40px;"),
-        div(numericInput(inputId = paste0("fac_max", i), label = h4("Maximum"),
-                  value = 1), style = "display: inline-block; padding-right: 40px;"),
-        br(),
-        br()
+                         "Float" = 2))),
+        column(width = 4,
+        numericInput(inputId = paste0("fac_min", i), label = h4("Minimum"),
+                  value = 0)),
+        column(width = 4,
+        numericInput(inputId = paste0("fac_max", i), label = h4("Maximum"),
+                  value = 1)),
+        align = "center"
 )
     })
   })
+  
+  # Generate hypercube
+  
+  eventReactive(input$genButton, {
+    # Sample a random 32 bit int as a seed for the LHC generation
+    lhc_seed <- sample(0:.Machine$integer.max, 1)
+    
+    lhc <- lhs.design(
+      nruns = as.integer(trunc(input$nruns)),
+      nfactors = as.integer(input$nfactors),
+      type = input$lhctype,
+      factor.names = list(
+        param1 = c(0.0, 1.0),
+        param2 = c(100, 20000)),
+      seed = lhc_seed
+    )
+    
+  })
+  
+  # Save output
+  
+  eventReactive(input$saveButton, {
+    print("saved")
+    write.csv(input$lhc, input$Filepath)
+  })
+  
 }
 
 
